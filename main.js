9156a068b2be8addcd07c9df2a76c43cd6b513ff
mukul47 <mukul47@github.com>
2021-07-12 23:46:44 +0530

    mouse controls added+

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 "use strict";
 const ConnectFour = (() => {
+  const overlayOpacity = 45;
+  const winnerCoordinatesStyle = "2px solid";
   const keyCode = { leftButton: 37, rightButton: 39, space: 32 };
+  const rowMap = { 0: 30, 1: 110, 2: 190, 3: 270, 4: 350, 5: 430, 6: 510 };
   const playerInControlColor = ["#ff0000", "#0000ff"];
   const defaultNodeColor = "#8888887e";
   let nodes = getDefaultNodes();
@@ -36,20 +39,40 @@ const ConnectFour = (() => {
     }
   }
 
+  function dropDisk() {
+    lastActivePosition = fillConnectFourGround();
+    if (lastActivePosition?.stopDisk) return;
+    checkWinner(playerInControl);
+    setPlayerInControl(true);
+    fillConnectFourGround(1);
+  }
   function listenToControl() {
     window.addEventListener("keyup", (e) => {
       const code = e.keyCode;
       if (code === keyCode.leftButton || code === keyCode.rightButton) {
         moveNodeTarget(code === keyCode.leftButton);
-        fillConnectFourGroundOverlay();
+        fillConnectFourGround(1);
       }
-      if (code === keyCode.space) {
-        lastActivePosition = fillConnectFourGround();
-        checkWinner(playerInControl);
-        setPlayerInControl(true);
-        fillConnectFourGroundOverlay();
-      }
+      if (code === keyCode.space) dropDisk();
     });
+    document
+      .querySelector(".main_header-playground")
+      .addEventListener("mouseover", (e) => {
+        if (e.target?.id) {
+          const row = `${e.target.id}`.split("")[0];
+          rowTarget = rowMap[row];
+          rowTargetIndex = row;
+          rowTargetElement.style.left = `${rowTarget}px`;
+          fillConnectFourGround(1);
+        }
+      });
+    document
+      .querySelector(".main_header-playground")
+      .addEventListener("click", (e) => {
+        if (e.target?.id) {
+          dropDisk();
+        }
+      });
   }
 
   function setPlayerInControl(action) {
@@ -61,36 +84,30 @@ const ConnectFour = (() => {
     playerInControlElement.innerText = `Player ${playerInControl + 1} turn!`;
   }
 
-  function fillConnectFourGround() {
+  function fillConnectFourGround(isOverlay) {
     const columnIndex = rowTargetIndex;
     const nodeColumn = nodes[columnIndex];
     const lastIndex = nodeColumn.findIndex((v) => v);
-    if (lastIndex === 0) return;
+    if (lastIndex === 0) return { stopDisk: true };
     const index = lastIndex === -1 ? 5 : lastIndex - 1;
-    nodeColumn[index] = `${playerInControl}`;
     const targetIndex = columnIndex + "" + index;
-    document.getElementById(targetIndex).style.background =
-      playerInControlColor[playerInControl];
-    return targetIndex.split("");
-  }
-
-  function fillConnectFourGroundOverlay() {
-    const columnIndex = rowTargetIndex;
-    const nodeColumn = nodes[columnIndex];
-    const lastIndex = nodeColumn.findIndex((v) => v);
-    if (lastIndex === 0) return;
-    if (lastTargetOverlay) {
-      const [x, y] = lastTargetOverlay.split("");
-      if (!nodes[x][y]) {
-        document.getElementById(lastTargetOverlay).style.background =
-          defaultNodeColor;
+    let isOverlayOpacity = "";
+    if (isOverlay) {
+      isOverlayOpacity = overlayOpacity;
+      if (lastTargetOverlay) {
+        const [x, y] = lastTargetOverlay.split("");
+        if (!nodes[x][y]) {
+          document.getElementById(lastTargetOverlay).style.background =
+            defaultNodeColor;
+        }
       }
+      lastTargetOverlay = targetIndex;
+    } else {
+      nodeColumn[index] = `${playerInControl}`;
     }
-    const index = lastIndex === -1 ? 5 : lastIndex - 1;
-    const idx = columnIndex + "" + index;
-    lastTargetOverlay = idx;
-    document.getElementById(idx).style.background =
-      playerInControlColor[playerInControl] + "30";
+    document.getElementById(targetIndex).style.background =
+      playerInControlColor[playerInControl] + isOverlayOpacity;
+    return targetIndex.split("");
   }
 
   function checkWinner(player) {
@@ -117,6 +134,7 @@ const ConnectFour = (() => {
   function checkDiagonal(x, y, player, topRight_bottom) {
     //top-left to bottom-right
     let count = 0;
+    let matchedCoordinates = [];
     while (1) {
       if (
         y >= 6 ||
@@ -128,9 +146,11 @@ const ConnectFour = (() => {
       const node = nodes[x][y];
       const hasMatch = `${node}` == player;
       if (hasMatch) {
-        if (++count === 4) someoneWon(player);
+        matchedCoordinates.push({ x, y });
+        if (++count === 4) someoneWon(player, matchedCoordinates);
       } else {
         count = 0;
+        matchedCoordinates = [];
       }
       if (!topRight_bottom) {
         x++;
@@ -140,7 +160,14 @@ const ConnectFour = (() => {
       y++;
     }
   }
-  function someoneWon(player) {
+  function someoneWon(player, coordinates) {
+    if (coordinates) {
+      coordinates.forEach((coordinate) => {
+        document.getElementById(
+          `${coordinate.x}${coordinate.y}`
+        ).style.outline = winnerCoordinatesStyle;
+      });
+    }
     setTimeout(() => {
       alert(`Player ${player + 1} won !!!`);
       setTimeout(() => {
@@ -201,6 +228,7 @@ const ConnectFour = (() => {
       for (let j = 0; j < 7; j++) {
         const element = document.getElementById(`${j}${i}`);
         element.style.background = defaultNodeColor;
+        element.style.outline = "none";
       }
     }
     setPlayerInControl(true);
@@ -211,13 +239,13 @@ const ConnectFour = (() => {
     setPlayerInControl();
     initalizeNodes();
     listenToControl();
-    fillConnectFourGroundOverlay();
+    fillConnectFourGround(1);
   }
 
   main.prototype.reset = function () {
     reset();
     setPlayerInControl(true);
-    fillConnectFourGroundOverlay();
+    fillConnectFourGround(1);
   };
 
   return main;
